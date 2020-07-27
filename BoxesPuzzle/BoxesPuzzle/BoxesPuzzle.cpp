1529623372b64139584d4d72e90d96ee8ba315b5
andrew <muzik.drew@gmail.com>
2020-07-26 20:11:43 -0700

    Initial Commit

---
+++ BoxesPuzzle/BoxesPuzzle/BoxesPuzzle.cpp
@@ -0,0 +1,81 @@
+// BoxesPuzzle.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <iostream>
+
+int main()
+{
+
+	int n;
+	scanf("%d", &n);
+
+	// square matrix -> length = width
+	int length = n * 2 - 1;
+	int *arr[length];
+	// create matrix
+	for (int i = 0; i < length; i++) {
+		arr[i] = (int *)malloc(length * sizeof(int));
+	}
+
+	// fill up matrix in "boxes" -> less optimal from frequent array access
+	// in favor of intuitive functionality
+
+	/* sample to fill up by "boxes" w/ n = 3
+			for(int i = 0; i < length; i++){
+			arr[0][i] = n;          // fill first row
+			arr[i][length-1] = n;     // fill last column
+			arr[length-1][i] = n;     // fill last row
+			arr[i][0] = n;          // fill first column
+		}
+		for(int i = 1; i < length - 1; i++){
+			arr[1][i] = n - 1;       // fill first row
+			arr[i][length-2] = n-1;     // fill last column
+			arr[length-2][i] = n-1;     // fill last row
+			arr[i][1] = n-1;          // fill first column
+		}
+
+		arr[n-1][n-1] = n - 2;
+	*/
+
+
+	int sub = 0;
+	int box = 0;
+	while (box < n) {
+		for (int i = box; i < length - box; i++) {
+			arr[box][i] = n - box;          // fill first row
+			arr[i][length - 1 - box] = n - box;     // fill last column
+			arr[length - 1 - box][i] = n - box;     // fill last row
+			arr[i][box] = n - box;          // fill first column
+		}
+		++box;
+	}
+	// fill center
+	arr[n - 1][n - 1] = n - box + 1;
+
+	for (int i = 0; i < length; i++) {
+		for (int j = 0; j < length; j++) {
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+
+
+	return 0;
+}
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
